636310b86c53e2b0cea0b35df3edc918dc3c5554
soumodiptaBo-Kore <soumodipta.bose@kore.com>
2023-10-03 13:46:08 +0530

    minor changes

--- 4-node-upgrade/scripts/bootstrap-al2023.sh
+++ 4-node-upgrade/scripts/bootstrap-al2023.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Deployment Script for KoreServer
 sudo yum update
 sudo yum install git htop -y # install dependencies
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 source ~/.bashrc
 chmod 600 ~/.ssh/id_rsa # fix permission for private key
+#install node 18
 nvm install 18
 nvm use 18
+npm install pm2 -g
 # configure github for getting code.
 git config --global user.name "soumodiptaBo-Kore"
 git config --global user.email "soumodipta.bose@kore.com"
@@ -15,4 +18,16 @@ mkdir kore
 cd kore
 git clone git@github.com:Koredotcom/KoreServer.git
 cd KoreServer
-npm install pm2 -g
+git checkout bots_dev
+git pull
+sudo adduser nginx
+sudo apt-get install nginx -y
+sudo mv /etc/nginx /etc/nginx-old
+sudo service nginx start
+cd /var
+sudo mkdir www
+sudo ln -s /home/ec2-user/kore/KoreServer/ /var/www
+sudo ln -s /home/ec2-user/kore/KoreServer/config/nginx/ /etc
+cd /home/ec2-user/kore/KoreServer/config/nginx/
+sudo ln -s integrations.conf nginx.conf
+sudo service nginx restart
--- 4-node-upgrade/scripts/bootstrap-redis.sh
+++ 4-node-upgrade/scripts/bootstrap-redis.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+yum update -y
+yum install htop -y
+sudo amazon-linux-extras install epel -y
+sudo yum update
+sudo yum install redis-server
+sudo yum install redis
+sudo sed -i 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis.conf
+sudo redis-server /etc/redis.conf
